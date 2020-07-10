bd92ea45733a362669779cda5bb23a513de1b9cc
enhao25 <larloller@gmail.com>
2020-07-10 21:02:02 +0800

    Allow accessing of contract data

--- src/App.js
+++ src/App.js
@@ -1,29 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
 import web3 from "./web3";
+import lottery from './lottery';
 
-function App() {
-  console.log(web3.version);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // Can use this instead of using the constructor
+  state = {
+    manager: '',
+    players: [],
+    balance: ''
+  }
+
+  async componentDidMount() {
+    const manager = await lottery.methods.manager().call();
+    const players = await lottery.methods.getPlayers().call();
+    // Balance inside our eth contract
+    const balance = await web3.eth.getBalance(lottery.options.address); 
+
+    this.setState({ manager, players, balance })
+  }
+
+  render() {
+    const { manager, players, balance } = this.state
+    return (
+      <div className="App">
+        <h2>Lottery Contract</h2>
+        <p>This Contract is managed by {manager}</p>
+        <p>There are currently {players.length} people entered, competiting to win {web3.utils.fromWei(balance, 'ether')} ether!</p>
+      </div>
+    );
+  }
 }
 
 export default App;

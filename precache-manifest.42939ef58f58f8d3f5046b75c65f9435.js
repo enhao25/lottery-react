422e1b9f199ec641f12c68060dbac5752c4eb767
enhao25 <larloller@gmail.com>
2020-08-01 09:06:57 +0800

    Updates

---
+++ precache-manifest.42939ef58f58f8d3f5046b75c65f9435.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b7dc030f6dcf12ddebbca33d6b65fd1",
+    "url": "/lottery-react/index.html"
+  },
+  {
+    "revision": "3cef2eff054c9e8c48f6",
+    "url": "/lottery-react/static/css/main.40e47ac8.chunk.css"
+  },
+  {
+    "revision": "fb60f28dccbc6af6e77c",
+    "url": "/lottery-react/static/js/2.63e8c7d6.chunk.js"
+  },
+  {
+    "revision": "08c1b7705eddcf469f4452ea65677df4",
+    "url": "/lottery-react/static/js/2.63e8c7d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cef2eff054c9e8c48f6",
+    "url": "/lottery-react/static/js/main.0846f0fd.chunk.js"
+  },
+  {
+    "revision": "e7f1ab669dc23b4ec97a",
+    "url": "/lottery-react/static/js/runtime-main.d2085d72.js"
+  }
+]);
